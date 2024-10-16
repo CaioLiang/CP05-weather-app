bdb8bfcb4adfbaa7dca9e364d86a264618581dd3
Caio Liang <caioliang2@gmail.com>
2024-10-16 01:49:33 -0300

    estilizando o Menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,25 +1,26 @@
 import Link from "next/link";
 import { BiHomeAlt, BiSearch, BiUserCircle, BiHeart } from "react-icons/bi";
+import style from './Menu.module.css'
 
 export const Menu = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className={style.styled_nav}>
+      <ul className={style.styled_ul} >
+        <li className={style.styled_li}>
           <BiHomeAlt size="1.5rem" color="#38aede" />
-          <Link href="/">Inicio</Link>
+          <Link className={style.styled_link}href="/">Inicio</Link>
         </li>
-        <li>
+        <li className={style.styled_li}>
           <BiSearch size="1.5rem" color="#38aede" />
-          <Link href="/busca">Busca</Link>
+          <Link className={style.styled_link} href="/busca">Busca</Link>
         </li>
-        <li>
+        <li className={style.styled_li}>
           <BiHeart size="1.5rem" color="#38aede" />
-          <Link href="/favorites">Favoritos</Link>
+          <Link className={style.styled_link} href="/favorites">Favoritos</Link>
         </li>
-        <li>
+        <li className={style.styled_li}>
           <BiUserCircle size="1.5rem" color="#38aede" />
-          <Link href="/perfil">Perfil</Link>
+          <Link className={style.styled_link} href="/perfil">Perfil</Link>
         </li>
       </ul>
     </nav>
