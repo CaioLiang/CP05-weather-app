7edd8afdd09ee90d9deb169d92e9c9acce0ae552
Caio Liang <caioliang2@gmail.com>
2024-10-16 22:52:33 -0300

    projeto semi funcional

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -2,12 +2,35 @@
 
 import { Layout } from "@/components/Layout/Layout";
 
+import { useRouter } from "next/navigation";
+import { Header } from "../../components/Header/Header";
+import UserContext from "../../context/UserContext";
+import { useVerifyLogin } from "../../helpers/useVerifyLogin";
+import { jwtDecode } from "jwt-decode";
+import { useContext, useEffect } from "react";
 
 export default function Perfil() {
+  
+  const router = useRouter();
+
+  useVerifyLogin();
+  const { userName, setUserName } = useContext(UserContext);
+
+  useEffect(() => {
+    const userToken = JSON.parse(sessionStorage.getItem("userToken"));
+
+    if (userToken) {
+      const userData = jwtDecode(userToken.token);
+      setUserName(userData.name);
+    } else {
+      router.push('/login');
+    }
+  }, [])
     return (
       <main>
         <Layout>
           <h1>Perfil</h1>
+          <Header title="Perfil" userName={userName} />
         </Layout>
       </main>
     )
